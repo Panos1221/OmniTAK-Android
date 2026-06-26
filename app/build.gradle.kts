1d5901ece88467d1922a2e5a9bf2617ce6f0147a
jfuginay <j@engindearing.soy>
2026-06-25 17:46:15 -0700

    chore(release): OmniTAK Android 0.40.0 (versionCode 101)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "soy.engindearing.omnitak.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 100
-        versionName = "0.39.2"
+        versionCode = 101
+        versionName = "0.40.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
